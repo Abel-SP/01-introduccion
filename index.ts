647727f547e72300ec1752af07949714268aa3be
Abel.164 <abelspiridione@gmail.com>
2022-12-11 21:45:33 -0300

    initial comit

---
+++ index.ts
@@ -0,0 +1,27 @@
+let numer1:number = 1;
+let numer2:number = 2;
+let numer3:number = 3;
+let numer4:number = 4;
+let numer5:number = 5;
+let numer6:number = 6;
+let numer7:number = 7;
+let numer8:number = 8;
+let numer9:number = 9;
+let numer10:number = Math.random();
+let nesa:any;
+
+console.log("divicion = " + numer8/numer2);
+console.log(numer8/numer3);
+console.log(numer1 + numer1);
+console.log(numer1 - numer1);
+console.log( numer4**(numer4/numer2) );
+console.log(numer1 * numer10);
+console.log();
+
+function decirHola( texto:string ){ // Declarar funcion
+    console.log(texto);
+}
+
+let mensaje:string = "Hola";
+decirHola(mensaje); // Ejecutar, instanciar funcion
+
---
+++ output(Carp.a.comp.)/index.js
@@ -0,0 +1,23 @@
+"use strict";
+let numer1 = 1;
+let numer2 = 2;
+let numer3 = 3;
+let numer4 = 4;
+let numer5 = 5;
+let numer6 = 6;
+let numer7 = 7;
+let numer8 = 8;
+let numer9 = 9;
+let numer10 = Math.random();
+let nesa = console.log("divicion = " + numer8 / numer2);
+console.log(numer8 / numer3);
+console.log(numer1 + numer1);
+console.log(numer1 - numer1);
+console.log(numer4 ** (numer4 / numer2));
+console.log(numer1 * numer10);
+console.log();
+function decirHola(texto) {
+    console.log(texto);
+}
+let mensaje = "Hola";
+decirHola(mensaje); // Ejecutar, instanciar funcion
